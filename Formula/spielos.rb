8f4b92461d164f066d9de6524e91201c3ff76bf4
ShayanSpiel <xhayan@gmail.com>
2026-09-13 01:51:33 +0330

    spielos 10.3.3

--- Formula/spielos.rb
+++ Formula/spielos.rb
@@ -10,8 +10,8 @@ class Spielos < Formula
   desc "AI company operating system with durable Goals, Departments, and approvals"
   homepage "https://spielos.xyz"
   # Content-addressed source sdist published by PyPI.
-  url "https://files.pythonhosted.org/packages/d1/8c/b8b78595236f49f4d57e24a2ed4272c744ab9d6ecc679643a64093a5485e/spielos-10.3.1.tar.gz"
-  sha256 "9aebd95573d10c16b3593807776c94bd9fe49f6b8f2d8c279fc4f00fc35b1142"
+  url "https://files.pythonhosted.org/packages/10/98/90b3df7d260b1e251f9df45feafd5f78d9bfc2ba0e5b4697f7005fe2118b/spielos-10.3.3.tar.gz"
+  sha256 "77eb93fd139c41115189b3c7ddcad61d4f03624da7eefb74c2091746a0ab8d59"
   license "MIT"
   head "https://github.com/ShayanSpiel/SpielOS.git", branch: "main"
 
